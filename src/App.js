259303f995505421a0d6abea8afd560fa06c9b53
Abdul Manan <abdulmanankam@gmail.com>
2023-07-16 10:27:21 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+function App() {
+  return (
+    <div className="App">
+      Weather Forcasting App
+    </div>
+  );
+}
+
+export default App;
